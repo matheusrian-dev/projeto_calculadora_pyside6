95cad80934a7d63b6d2ede46f40c3d55649ca52a
Matheus R. Souza <matheusrian.dev@gmail.com>
2025-05-20 11:57:04 -0300

    Adicionando o módulo styles.py e aprendendo sobre o QLineEdit.

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
 import sys
 
-from PySide6.QtWidgets import (
-    QApplication,
-    QLabel,
-)
+from PySide6.QtWidgets import QApplication, QLabel
+from display import Display
 from PySide6.QtGui import QIcon
 from main_window import MainWindow
 from paths import WINDOW_ICON_PATH_STR
@@ -19,11 +17,17 @@ if __name__ == '__main__':
     app = QApplication(sys.argv)
     window = MainWindow()
 
+    # É possível settar um texto inicial ao inserir
+    # string no Display()
+    display = Display()
+    display.setPlaceholderText('Digite alguma coisa')
+    window.addToVLayout(display)
+
     icon = QIcon(WINDOW_ICON_PATH_STR)
     window.setWindowIcon(icon)
     app.setWindowIcon(icon)
 
-    window.addWidgetToVLayout(temp_label('Texto 1'))
+    window.addToVLayout(temp_label('Texto 1'))
 
     window.adjustFixedSize()
     window.show()
--- main_window.py
+++ main_window.py
@@ -24,5 +24,5 @@ class MainWindow(QMainWindow):
     # Não é uma boa prática manter acessos aninhados,
     # caso veja que há mais de um (objeto1.objeto2.funcao1),
     # crie um método para implementar esse acesso.
-    def addWidgetToVLayout(self, widget: QWidget):
+    def addToVLayout(self, widget: QWidget):
         self.v_layout.addWidget(widget)
